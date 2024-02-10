f41c105b6f1479c918a064a92d93dd7fe8539ddc
Mateusz Stalmach <mateusz.stalmach@10g.pl>
2024-02-10 15:21:42 +0100

    Poprawki do ceny

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,17 +1,23 @@
-import React from "react";
-import { Row } from "react-bootstrap";
+import React from "react"
+import { Row } from "react-bootstrap"
 
 const Footer = () => (
   <footer>
     <Row className="w-100">
-      <div className="col-md-6 text-muted text-center">
+      <div
+        className="col-md-6 text-muted text-center"
+        style={{ fontSize: "0.8rem" }}
+      >
         Stworzona przez:&ensp;Stalmach Mateusz
       </div>
-      <div className="col-md-6 text-muted text-center">
+      <div
+        className="col-md-6 text-muted text-center"
+        style={{ fontSize: "0.8rem" }}
+      >
         Telefon kontaktowy do twórcy:&ensp;669157750
       </div>
     </Row>
   </footer>
-);
+)
 
-export default Footer;
+export default Footer
